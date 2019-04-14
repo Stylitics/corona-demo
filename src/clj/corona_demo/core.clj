deeb04ab10809742402e4d2e59b44d3780f94c4b
Leon Talbot <leontalbot@gmail.com>
2019-04-14 08:00:20 -0400

    adding solr core and schema

--- src/clj/corona_demo/core.clj
+++ src/clj/corona_demo/core.clj
@@ -1,5 +1,9 @@
 (ns corona-demo.core
-  (:require [corona-demo.data :as data]))
+  (:gen-class)
+  (:require
+   [corona-demo.data :as data]
+   [corona.client :as solr.client]
+   [corona.schema :as solr.schema]))
 
 ;;; 1. Get Movies Dataset
 
@@ -7,3 +11,89 @@
 
 #_(first data/movies)
 #_(count data/movies)
+
+
+
+
+
+;;; 2. Setup Solr Core
+
+;; For emacs user, see corona doc/Installation.md
+
+(def core-dir      (str (System/getProperty "user.dir") "/resources/solr/tmdb"))
+(def conf-dir      (str core-dir "/conf"))
+(def data-dir      (str core-dir "/data"))
+(def client-config {:type :http :core :tmdb})
+
+(defn core-reset!
+  []
+  (println "SOLR: Deleting :tmdb core...")
+  ;; same as $SOLR_HOME/bin/solr delete -c tmdb
+  (solr.client/delete-core! client-config {:delete-index? true})
+
+  (println "SOLR: Creating back :tmdb core...")
+  ;; same as $SOLR_HOME/bin/solr create -c tmdb -d conf-dir
+  (solr.client/create-core! client-config {:instance-dir core-dir})
+
+  (println "SOLR: Ready to add and index documents :-)"))
+
+#_(core-reset!)
+
+
+
+
+
+;;; 3. Add Solr Schema Types
+
+(def custom-schema data/schema-type-text_en_splitting)
+
+(defn get-types-fields
+  []
+  (println "SOLR: Fetching actual field types")
+  (solr.schema/get-field-types client-config))
+
+(defn add-schema-types!
+  []
+  (println "SOLR: adding schema types")
+  (solr.schema/add-field-type! client-config custom-schema))
+
+;; if you just pulled this demo, all field types are already in manageschema
+;; thus, and no action is required.
+
+#_(add-schema-types!)
+
+
+
+;;; 4. Add Solr Schema Fields
+
+(defn get-fields []
+  (println "SOLR: Fetching actual fields")
+  (solr.schema/get-fields client-config))
+
+(defn add-schema-fields!
+  "Writes to resources/solr/tmdb/conf/manageschema adding new <field> lines."
+  [new-schema-fields]
+  (println "SOLR: adding schema fields")
+  (solr.schema/add-field! client-config new-schema-fields))
+
+(defn replace-schema-fields!
+  "Writes to resources/solr/tmdb/conf/manageschema replacing existing <field> lines."
+  [schema-fields]
+  (println "SOLR: replacing schema fields")
+  (solr.schema/replace-field! client-config schema-fields))
+
+(def schema-fields (concat data/basic-fields data/content-fields data/number-fields))
+
+;; if you just pulled this demo, all fields are already in manageschema
+;; thus, and no action is required.
+#_(add-schema-fields! [{}])
+#_(replace-schema-fields! schema-fields)
+
+
+
+
+
+
+(defn -main
+  []
+  (println "running collage-demo.core/-main"))
--- src/clj/corona_demo/data.clj
+++ src/clj/corona_demo/data.clj
@@ -44,3 +44,231 @@
 (defonce credits (map parse-credit-raw-map credits-raw-maps))
 
 (defonce movies (utils/merge-by :db_id movies* credits))
+
+
+
+
+(def schema-type-text_en_splitting
+  "A text field with defaults appropriate for English, plus
+  aggressive word-splitting and autophrase features enabled.
+  This field is just like text_en, except it adds
+  WordDelimiterGraphFilter to enable splitting and matching of
+  words on case-change, alpha numeric boundaries, and
+  non-alphanumeric chars.  This means certain compound word
+  cases will work, for example query \"wi fi\" will match
+  document \"WiFi\" or \"wi-fi\".
+  "
+  {:name "text_en_splitting"
+   :class "solr.TextField"
+   :positionIncrementGap "100"
+   :autoGeneratePhraseQueries "true"
+   :indexAnalyzer {:tokenizer {:class "solr.WhitespaceTokenizerFactory"}
+                   :filters [
+                             ;; Removes stop words
+                             {:class "solr.StopFilterFactory"
+                              :words "lang/stopwords_en.txt" ;added
+                              :ignoreCase "true"}
+
+                             ;; Splits and matches words on case-change,
+                             ;; alpha numeric boundaries, and
+                             ;; non-alphanumeric chars
+                             ;; 'wi fi' = 'WiFi' = 'wi-fi'
+                             {:class "solr.WordDelimiterGraphFilterFactory"
+                              :generateWordParts "1"
+                              :generateNumberParts "1"
+                              :catenateWords "1"
+                              :catenateNumbers "1"
+                              :catenateAll "0"
+                              :splitOnCaseChange "1"}
+
+                             ;; Lowercases content
+                             {:class "solr.LowerCaseFilterFactory"}
+
+                             ;; Protects some words from being stemmed
+                             {:class "solr.KeywordMarkerFilterFactory"
+                              :protected "protwords.txt"}
+
+                             ;; Applies the Porter Stemming Algorithm for English
+                             ;; "jump" "jumping" "jumped" => "jump" "jump" "jump"
+                             {:class "solr.PorterStemFilterFactory"}
+
+                             ;; Needed for synonyms
+                             {:class "solr.FlattenGraphFilterFactory"}]}
+    :queryAnalyzer {:type "query"
+                    :tokenizer {:class "solr.WhitespaceTokenizerFactory"}
+
+                    :filters [ ;; Adds up in synonyms at query time
+                              {:class "solr.SynonymGraphFilterFactory"
+                               :synonyms "synonyms.txt" ;added
+                               :ignoreCase "true"
+                               :expand "true"}
+                              {:class "solr.StopFilterFactory"
+                               :words "lang/stopwords_en.txt" ;added
+                               :ignoreCase "true"}
+                              {:class "solr.WordDelimiterGraphFilterFactory"
+                               :generateWordParts "1"
+                               :generateNumberParts "1"
+                               :catenateWords "1"
+                               :catenateNumbers "1"
+                               :catenateAll "0"
+                               :splitOnCaseChange "1"}
+                              {:class "solr.LowerCaseFilterFactory"}
+                              {:class "solr.KeywordMarkerFilterFactory"
+                               :protected "protwords.txt" ;added
+                               }
+                              {:class "solr.PorterStemFilterFactory"}
+                              ]}})
+
+
+(def basic-fields
+  "You can find your basic schema types here:
+  resources/solr/tmdb/conf/managedschema"
+  [{:name "_text_",
+    :type "text_en_splitting",
+    :multiValued true,
+    :indexed true,
+    :stored false}
+
+   {:name "_version_", ; version of document, query per version
+    :type "plong",
+    :indexed true,
+    :stored true}
+
+   {:name "db_id",
+    :type "string",
+    :multiValued false,
+    :indexed true,
+    :required true,
+    :stored true}])
+
+(def content-fields
+  [{:name "tagline",
+    :type "text_en_splitting",
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "overview",
+    :type "text_en_splitting",
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "genres",
+    :type "text_lowcased",
+    :multiValued true,
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "title",
+    :type "text_lowcased",
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "keywords",
+    :type "text_en_splitting",
+    :indexed true,
+    :stored true
+    :multiValued true
+    :termVectors true}
+
+   {:name "original_title",
+    :type "text_lowcased",
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "production_companies",
+    :type "text_lowcased",
+    :multiValued true,
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "production_countries",
+    :type "text_lowcased",
+    :multiValued true,
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "spoken_languages",
+    :type "text_lowcased",
+    :multiValued true,
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "original_language",
+    :type "text_lowcased",
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "status",
+    :type "text_lowcased",
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "homepage",
+    :type "text_lowcased",
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "cast",
+    :type "text_lowcased",
+    :indexed true,
+    :stored true
+    :multiValued true
+    :termVectors true}
+
+   {:name "director",
+    :type "text_lowcased",
+    :indexed true,
+    :stored true
+    :termVectors true}
+
+   {:name "producers",
+    :type "text_lowcased",
+    :indexed true,
+    :stored true
+    :multiValued true
+    :termVectors true}])
+
+(def number-fields
+  [{:name "release_date",
+    :type "pdate",
+    :indexed true,
+    :stored true}
+
+   {:name "budget",
+    :type "plong",
+    :indexed true,
+    :stored true}
+
+   {:name "revenue",
+    :type "plong",
+    :indexed true,
+    :stored true}
+
+   {:name "vote_count",
+    :type "plongs"}
+
+   {:name "popularity",
+    :type "pfloat",
+    :indexed true,
+    :stored true}
+
+   {:name "runtime",
+    :type "pfloat",
+    :indexed true,
+    :stored true}
+
+   {:name "vote_average",
+    :type "pfloat",
+    :indexed true,
+    :stored true}])
